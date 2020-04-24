37d7d79dc39881b1e85feef4eeda2482d6f98573
Yash Karandikar <ynkarandikar@gmail.com>
2020-04-23 19:19:53 -0700

    find max frequencies of k-mers in provided strings

--- replication.py
+++ replication.py
@@ -54,3 +54,29 @@ def frequency_given_k(string, k):
         elif string_array_fn[b] in dict.keys():  # if the checked 3-mer IS in the keys, i.e. it has been spotted before
             dict[string_array_fn[b]] = dict[string_array_fn[b]] + 1  # increment the frequency it already has, we have spotted another occurrence
     return dict
+
+# function that finds the max frequency 
+def find_max_frequency_of_words(dict_provided):
+    max_freq = max(dict_provided.values())
+    print(max_freq)
+
+    # get dictionary key directly as a variable
+    # https://stackoverflow.com/questions/3545331/how-can-i-get-dictionary-key-as-variable-directly-in-python-not-by-searching-fr
+    frequent_text = []
+    for key, value in dict_provided.items():
+        if value == max_freq: # is the frequency of the current k-length string equal to the max frequency?
+            frequent_text.append(key) # if yes, note the k-length string for which that is true
+    print(frequent_text)
+
+# print max frequencies of various k-length strings
+# wrap the frequency_given_k function into max_frequency to use the dictionary from the first function
+	# function composition is necessary to pass variables without class instantiation
+find_max_frequency_of_words(frequency_given_k('CGATATATCCATAG', 3))
+find_max_frequency_of_words(frequency_given_k('ACGTTGCATGTCGCATGATGCATGAGAGCT', 4))
+
+# apply solutions to Frequent Words Problem - Text = oriC of cholerae and k = 10
+frequent_words_problem_text = cholerae_str
+frequent_words_problem_k = 10
+
+find_max_frequency_of_words(frequency_given_k(frequent_words_problem_text, frequent_words_problem_k))
+print('\n')
